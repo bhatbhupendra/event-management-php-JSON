cf2ae4e3325004b0703380f3e75438ab68c476f8
Bhupendra Bhat <bhupendra420110bhat@gmail.com>
2024-06-09 23:54:16 +0545

    Update: login.class

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     if ($_SERVER['REQUEST_METHOD'] == "POST"){  
         $email = trim($_POST['email']);     
         $password = trim($_POST['password']);    
-        $user = new LoginUser($emai, $password);
+        $user = new LoginUser($email, $password);
     }
 ?>
 
